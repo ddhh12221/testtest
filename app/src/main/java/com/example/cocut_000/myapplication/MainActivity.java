4ec1c509791e8d5f23fb75ba062c28fdb0576109
sum <cocuter@naver.com>
2016-12-09 15:41:03 +0900

    2번에 3번 예약완료

--- app/src/main/java/com/example/cocut_000/myapplication/MainActivity.java
+++ app/src/main/java/com/example/cocut_000/myapplication/MainActivity.java
@@ -15,6 +15,7 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,11 +26,13 @@ public class MainActivity extends AppCompatActivity {
     RadioGroup RG1;
     RadioButton RB1, RB2, RB3;
     Switch SW1;
-    Button BT1;
+    Button BT1, BT2;
     TextView TV1, TV2, TV3,TV4,TV5,TV6,TV7;
     ImageView IV1;
     FrameLayout FL1;
 
+    int sum1;
+    double sum2, sum3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,6 +59,11 @@ public class MainActivity extends AppCompatActivity {
         RB2=(RadioButton)findViewById(R.id.radioButton2);
         RB3=(RadioButton)findViewById(R.id.radioButton2);
         IV1=(ImageView)findViewById(R.id.imageView);
+        ET3=(EditText)findViewById(R.id.editText3);
+        ET2=(EditText)findViewById(R.id.editText2);
+        ET1=(EditText)findViewById(R.id.editText);
+        BT1=(Button)findViewById(R.id.button2);
+        BT2=(Button)findViewById(R.id.button);
 
 
 
@@ -89,6 +97,45 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        BT1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ET3.getText() != null && ET2.getText() != null && ET1.getText() != null) {
+                    double pr1 = Double.parseDouble(ET3.getText().toString());
+                    double pr2 = Double.parseDouble(ET2.getText().toString());
+                    double pr3 = Double.parseDouble(ET1.getText().toString());
+
+
+                    sum1 = Integer.parseInt(ET1.getText().toString()) + Integer.parseInt(ET2.getText().toString()) + Integer.parseInt(ET3.getText().toString());
+
+                    TV5.setText(Integer.toString(sum1));
+                    sum2 = (pr1 * 15000) + (pr2 * 12000) + (pr3 * 8000);
+                    sum3 =sum2;
+                    if (RB1.isChecked()) {
+                        sum3 =sum3 * 0.05;
+                        TV6.setText(Double.toString(sum3));
+                        sum2 = sum2 * 0.95;
+                        TV7.setText(Double.toString(sum2));
+                    } else if (RB2.isChecked()) {
+                        sum3 =sum3 * 0.10;
+                        TV6.setText(Double.toString(sum3));
+                        sum2 = sum2 * 0.90;
+                        TV7.setText(Double.toString(sum2));
+                    } else if (RB3.isChecked()) {
+                        sum3 =sum3 * 0.20;
+                        TV6.setText(Double.toString(sum3));
+                        sum2 = sum2 * 0.80;
+                        TV7.setText(Double.toString(sum2));
+                    }
+                }
+                else{
+                    Toast.makeText(this, "인원을 입력하세요", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
 
 
     }
